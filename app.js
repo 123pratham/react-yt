f4981bb86de043a2d952420eae860aa59d3a4e58
Pratham Sahu <sahu.pratham123@gmail.com>
2026-03-16 10:09:53 +0530

    01-folder

---
+++ app.js
@@ -0,0 +1,3 @@
+const arr = [12,22,56,568,8555,568]
+
+export default arr
---
+++ script.js
@@ -0,0 +1,6 @@
+import arr from './app.js' 
+
+console.log(arr)
+// var h1 = document.createElement('h1')
+// h1.innerHTML= "hello from js"
+// document.body.appendChild(h1)
